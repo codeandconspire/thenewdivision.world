6476660311505895b5531edcce12a5450eb66022
Carl Törnqvist <calle.tornqvist@gmail.com>
2018-04-04 18:23:07 +0200

    Add card component

---
+++ components/card/index.js
@@ -0,0 +1,74 @@
+const css = require('sheetify')
+const assert = require('assert')
+const html = require('choo/html')
+const Component = require('choo/component')
+const { observe, mousemove } = require('../base')
+css('./index')
+
+const THEMES = {
+  white: 'u-bgWhite',
+  gray: 'u-bgGray',
+  brightBlue: 'u-bgBrightBlue',
+  petrol: 'u-bgPetrol',
+  darkBlue: 'u-bgDarkBlue',
+  sand: 'u-bgSand',
+  brightSand: 'u-bgBrightSand',
+  darkMagenta: 'u-bgDarkMagenta'
+}
+
+module.exports = class Card extends Component {
+  constructor (id, state, emit) {
+    super(id)
+    this.cache = state.cache
+    this.emit = emit
+  }
+
+  load (el) {
+    el = this.hasImage ? el.querySelector('.js-block') : el
+
+    const stopMouseMove = mousemove(el)
+    const stopObserving = observe(el, function () {
+      stopObserving()
+    })
+    this.unload = function () {
+      stopMouseMove()
+      stopObserving()
+    }
+  }
+
+  update () {
+    return false
+  }
+
+  createElement (props) {
+    assert(props.children, 'card: children should be defined')
+
+    const theme = props.color || 'white'
+    assert(typeof theme === 'string', 'card: color should be a string')
+
+    const hasImage = this.hasImage = !!props.image
+
+    const card = html`
+      <div class="${hasImage ? 'Card-block js-block' : 'Card'} ${THEMES[theme]}">
+        <div class="Card-decorator">
+          <svg class="Card-plus" width="20" height="20" viewBox="0 0 20 20">
+            <path fill="#EFECE5" fill-rule="evenodd" d="M12.5 7.5H20v5h-7.5V20h-5v-7.5H0v-5h7.5V0h5v7.5z"/>
+          </svg>
+        </div>
+        ${props.children}
+      </div>
+    `
+
+    if (!hasImage) return card
+
+    return html`
+      <div class="Card Card--hasImage">
+        <figure class="Card-figure">
+          <img class="Card-image" src="${props.image.url}" alt="${props.image.alt || ''}">
+          ${props.image.caption ? html`<figcaption class="Card-caption">${props.caption}</figcaption>` : null}
+        </figure>
+        ${card}
+      </div>
+    `
+  }
+}
--- views/about.js
+++ views/about.js
@@ -1,7 +1,8 @@
 const html = require('choo/html')
 const asElement = require('prismic-element')
-const { asText} = require('prismic-richtext')
+const { asText } = require('prismic-richtext')
 const view = require('../components/view')
+const Card = require('../components/card')
 const { i18n } = require('../components/base')
 const { Figure } = require('../components/link')
 const presentation = require('../components/presentation')
@@ -40,50 +41,85 @@ function about (state, emit) {
             </div>
           </div>
         </div>
-        <div class="Grid">
-          ${doc.data.coworkers.map((person, index, list) => {
-            const id = asText(person.name).trim().replace(/[^\w+]/, '-')
-            const children = [
-              html`
-                <article class="Grid-cell Grid-cell--1of3">
-                  ${state.cache(Figure, id).render(person.image)}
-                  <h3 class="Display Display--3">${asText(person.name)}</h3>
-                  <div class="Text">
-                    <p>${person.role}</p>
-                    ${asElement(person.bio)}
-                  </div>
-                </article>
-              `
-            ]
-
-            if (index === Math.floor((list.length - 1) / 2)) {
-              children.push(html`
-                <article class="Grid-cell Grid-cell--1of3">
-                  <div class="u-fill u-flex u-column u-bgPetrol u-colorWhite">
-                    <div class="u-fill u-flex u-column u-justifyCenter">
-                      <h3>
-                        <span class="Display Display--3">${text`Want a job?`}</span>
-                        <span class="Display Display--2">${doc.data.recruitment_heading}</span>
-                      </h3>
-                    </div>
-                    <h4 class="Display Display--3">${text`Careers`}</h4>
-                    <div class="Text Text--large">
-                      <p>
-                        <a href="mailto:hannah@thenewdivision.world">hannah@thenewdivision.world</a>
-                        <a href="tel:+46701234567">+46 (0)70 123 45 67</a>
-                      </p>
-                    </div>
-                  </div>
-                </article>
-              `)
-            }
-
-            return children
-          })}
+        <div class="Grid u-spaceBl">
+          ${doc.data.coworkers.map(coworker(state, doc))}
         </div>
+        ${state.cache(Card, 'workspace').render({
+          color: 'darkBlue',
+          image: doc.data.workspace_image,
+          caption: doc.data.workspace_image.alt,
+          children: html`
+            <div class="u-fill u-flex u-column u-colorWhite">
+              <div class="u-fill u-flex u-column u-justifyCenter">
+                <h3>
+                  <span class="Display Display--3">${text`Address`}</span>
+                  <span class="Display Display--2">${doc.data.address[0].text}</span>
+                </h3>
+                <div class="Text">
+                  <p>${doc.data.address.slice(1).map(part => [part.text, html`<br>`])}</p>
+                </div>
+              </div>
+              <h4 class="Display Display--3">${text`Inquiries`}</h4>
+              <div class="Text Text--large">
+                <p>
+                  <a href="mailto:hello@thenewdivision.world">hello@thenewdivision.world</a>
+                </p>
+              </div>
+            </div>
+          `
+        })}
       </section>
     </main>
   `
+}
+
+function coworker (state, doc) {
+  return function (person, index, list) {
+    const id = asText(person.name).trim().replace(/[^\w+]/, '-')
+    const children = [
+      html`
+        <article class="Grid-cell Grid-cell--1of3">
+          ${state.cache(Card, id).render({children: html`
+            <img class="u-block" src="${person.image.url}" alt="${person.image.alt || ''}">
+          `})}
+          <h3 class="Display Display--3">${asText(person.name)}</h3>
+          <div class="Text">
+            <p>${person.role}</p>
+            ${asElement(person.bio)}
+          </div>
+        </article>
+      `
+    ]
+
+    if (index === Math.floor((list.length - 1) / 2)) {
+      children.push(html`
+        <article class="Grid-cell Grid-cell--1of3">
+          ${state.cache(Card, 'recruit').render({
+            color: 'petrol',
+            children: html`
+              <div class="u-flex u-column u-colorWhite">
+                <div class="u-fill u-flex u-column u-justifyCenter">
+                  <h3>
+                    <span class="Display Display--3">${text`Want a job?`}</span>
+                    <span class="Display Display--2">${doc.data.recruitment_heading}</span>
+                  </h3>
+                </div>
+                <h4 class="Display Display--3">${text`Careers`}</h4>
+                <div class="Text Text--large">
+                  <p>
+                    <a href="mailto:hannah@thenewdivision.world">hannah@thenewdivision.world</a>
+                    <a href="tel:+46701234567">+46 (0)70 123 45 67</a>
+                  </p>
+                </div>
+              </div>
+            `
+          })}
+        </article>
+      `)
+    }
+
+    return children
+  }
 }
 
 function title () {
