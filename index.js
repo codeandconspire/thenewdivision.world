0ef3a9b9a6c71614cd17983e8d52262eeee76d57
Carl Törnqvist <calle.tornqvist@gmail.com>
2018-04-04 16:19:32 +0200

    Fix perf warnings

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const choo = require('choo')
 
 const app = choo()
 
-if (process.env.NODE_ENV === 'development' && typeof window !== 'undefined') {
+if (process.env.NODE_ENV === 'development') {
+  if (typeof window !== 'undefined') {
   app.use(require('choo-devtools')())
   app.use(require('choo-service-worker/clear')())
+  } else {
+    global.perf_hooks.performance.maxEntries = 500
+  }
 }
 
 app.use(require('choo-service-worker')('/sw.js'))
