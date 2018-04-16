f10c6cc4cb58b925d9996df15ffc05c7680a6428
Carl Törnqvist <calle.tornqvist@gmail.com>
2018-04-16 17:08:48 +0200

    Simplified work parallax

--- components/words/index.js
+++ components/words/index.js
@@ -25,21 +25,21 @@ module.exports = class Words extends Component {
   load (element) {
     this.reflow()
 
-    let cols = parse()
+    let cols = measure()
     let prev = window.innerWidth
     const top = element.offsetTop
 
     const onscroll = nanoraf(() => {
-      const { scrollY, innerHeight } = window
+      const { scrollY } = window
 
-      if (scrollY + innerHeight < top) return
+      if (scrollY < top) return
 
       for (let i = 1, len = cols.length, col; i < len; i++) {
         col = cols[i]
-        if (innerHeight > col.height) continue
-        if (scrollY + innerHeight > top + col.height) continue
-        const inview = scrollY + innerHeight - top
-        col.el.style.setProperty('--offset', `${((inview / col.height) * col.overflow * -1).toFixed(2)}px`)
+        if (scrollY > top + col.height) continue
+        const inview = scrollY - top
+        const fraction = inview / col.height
+        col.el.style.setProperty('--offset', fraction.toFixed(6))
       }
     })
 
@@ -48,7 +48,7 @@ module.exports = class Words extends Component {
       if ((prev >= 1000 && next < 1000) || (prev < 1000 && next >= 1000)) {
         this.rerender()
         this.reflow()
-        cols = parse()
+        cols = measure()
         onscroll()
       }
       prev = next
@@ -61,30 +61,30 @@ module.exports = class Words extends Component {
       window.removeEventListener('resize', onresize)
     }
 
-    function parse () {
-      return [...element.querySelectorAll('.js-col')].map((el, i, list) => {
-        const height = el.offsetHeight
-        const overflow = height - list[0].offsetHeight
-        return { el, height, overflow }
-      })
+    function measure () {
+      return [...element.querySelectorAll('.js-col')].map((el, i, list) => ({
+        el: el,
+        height: el.offsetHeight
+      }))
     }
   }
 
   reflow () {
     const last = []
-    const cols = this.element.querySelectorAll('.js-col')
-    const fraction = Math.floor(cols[0].childElementCount * 0.25) || 1
+    const cols = [...this.element.querySelectorAll('.js-col')]
 
     for (let i = 0, len = cols.length; i < len; i++) {
-      last.push(...Array.prototype.slice.call(cols[i].childNodes, fraction * -1))
+      last.push(...Array.prototype.slice.call(cols[i].childNodes, -2))
+      cols[i].removeChild(cols[i].lastElementChild)
+      cols[i].removeChild(cols[i].lastElementChild)
     }
 
     for (let i = 0, len = last.length; i < len; i++) {
-      if (i < Math.floor(len / 3)) {
-        cols[2 % cols.length].appendChild(last[i])
-      } else {
-        cols[1].appendChild(last[i])
-      }
+      const shortest = cols.reduce((min, el) => {
+        const height = el.offsetHeight
+        return !min || height < min.height ? {el, height} : min
+      }, null)
+      shortest.el.appendChild(last[i])
     }
   }
 
@@ -105,7 +105,7 @@ module.exports = class Words extends Component {
     return html`
       <div class="Words">
         ${cols.map((cells, col) => html`
-          <div class="Words-col js-col">
+          <div class="Words-col Words-col--${col + 1} js-col">
             ${cells.map((slice, cell) => {
               const props = slice.primary
               switch (slice.slice_type) {
