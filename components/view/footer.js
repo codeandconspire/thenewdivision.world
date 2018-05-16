9bc99fdf3d8a9a3d79c39be6f0275539d67a0d35
Anton Trollbäck <anton@trollback.se>
2018-05-16 17:01:25 +0200

    Tweak emails

--- components/view/footer.js
+++ components/view/footer.js
@@ -19,31 +19,33 @@ module.exports = class Footer extends Component {
     this.theme = theme
     return html`
       <footer class="u-textCenter u-spaceT8">
-        <h2 class="u-spaceT8">
-          <span class="u-textSizeLg u-textBold u-block u-spaceT8 u-spaceB4">
-            ${text`Connect with us,`}
-          </span>
-          <span class="Display Display--footer">
-            ${text`Be a force for good`}<br>
-          </span>
-        </h2>
+        <div class="u-spaceT2">
+          <h2 class="u-spaceT8">
+            <span class="u-textSizeLg u-textBold u-block u-spaceT8 u-spaceB4">
+              ${text`Connect with us,`}
+            </span>
+            <span class="Display Display--footer">
+              ${text`Be a force for good`}<br>
+            </span>
+          </h2>
 
-        <div class="Display Display--footer u-spaceB8">
-          <a class="Display-link u-spaceB8" href="mailto:hello@thenewdivision.world">hello@thenewdivision.world</a>
-        </div>
+          <div class="Display Display--footer u-spaceB8">
+            <a class="Display-link u-spaceB8" href="mailto:hello@thenewdivision.world">hello@thenewdivision.world</a>
+          </div>
 
-        <div class="Text u-spaceV8 u-inlineBlock ${ICON_THEMES[theme]}">
-          <a href="#" class="u-inlineBlock u-spaceLch u-spaceRch">${icon.facebook()}</a>
-          <a href="#" class="u-inlineBlock u-spaceRch">${icon.twitter()}</a>
-          <a href="#" class="u-inlineBlock u-spaceRch">${icon.instagram()}</a>
-          <a href="#" class="u-inlineBlock u-spaceRch">${icon.linkedin()}</a>
-          <p class="u-textSizeXs u-spaceT4">
-            ${text`© 2018 The New Division by Trollbäck + Company.`}<br>
-            ${text`All Rights Reserved.`}
-            <br>
-            <br>
-            <a href="https://github.com/codeandconspire/thenewdivision.world">${text`This website is open-source.`}</a>
-          </p>
+          <div class="Text u-spaceV8 u-inlineBlock ${ICON_THEMES[theme]}">
+            <a href="#" class="u-inlineBlock u-spaceLch u-spaceRch">${icon.facebook()}</a>
+            <a href="#" class="u-inlineBlock u-spaceRch">${icon.twitter()}</a>
+            <a href="#" class="u-inlineBlock u-spaceRch">${icon.instagram()}</a>
+            <a href="#" class="u-inlineBlock u-spaceRch">${icon.linkedin()}</a>
+            <p class="u-textSizeXs u-spaceT4">
+              ${text`© 2018 The New Division by Trollbäck + Company.`}<br>
+              ${text`All Rights Reserved.`}
+              <br>
+              <br>
+              <a href="https://github.com/codeandconspire/thenewdivision.world">${text`This website is open-source.`}</a>
+            </p>
+          </div>
         </div>
       </footer>
     `
