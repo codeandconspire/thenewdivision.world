aeb2f4c1154a42079f69eea4e61ebb6d86e8505a
Carl Törnqvist <calle.tornqvist@gmail.com>
2018-05-16 10:13:31 +0200

    Fix html etag

--- bin/lib/render.js
+++ bin/lib/render.js
@@ -1,4 +1,5 @@
 var document = require('./document')
+var concat = require('concat-stream')
 var {performance} = require('perf_hooks')
 
 module.exports = render
@@ -7,24 +8,31 @@ function render (entry) {
   return async function render (ctx, next) {
     try {
       await next()
-      if (!ctx.body) {
+      if (!ctx.body && ctx.accepts('html')) {
         let app = require(entry)
         let state = Object.assign({}, await prefetch(app, ctx.path), ctx.state)
         let html = app.toString(ctx.path, state)
 
-        let cache = `s-maxage=${60 * 60 * 24 * 7}, max-age=${60 * 10}`
-        if (ctx.app.env === 'development') cache = 'max-age=0'
+        await new Promise(function (resolve, reject) {
+          var stream = document(html, app.state, ctx.app)
+          stream.pipe(concat({encoding: 'buffer'}, function (buff) {
+            ctx.body = buff
+            ctx.type = 'text/html'
 
-        ctx.body = document(html, app.state, ctx.app)
-        ctx.set('Cache-Control', cache)
-        ctx.append('Link', [
-          `</${ctx.script.hash.toString('hex').slice(0, 16)}/bundle.js>; rel=preload; as=script`,
-          `</${ctx.style.hash.toString('hex').slice(0, 16)}/bundle.css>; rel=preload; as=style`
-        ])
-        ctx.type = 'text/html'
+            let cache = `s-maxage=${60 * 60 * 24 * 7}, max-age=${60 * 10}`
+            if (ctx.app.env === 'development') cache = 'max-age=0'
+            ctx.set('Cache-Control', cache)
 
-        performance.clearMarks()
-        performance.clearMeasures()
+            ctx.append('Link', [
+              `</${ctx.script.hash.toString('hex').slice(0, 16)}/bundle.js>; rel=preload; as=script`,
+              `</${ctx.style.hash.toString('hex').slice(0, 16)}/bundle.css>; rel=preload; as=style`
+            ])
+
+            performance.clearMarks()
+            performance.clearMeasures()
+            resolve()
+          }))
+        })
       }
     } catch (err) {
       ctx.throw(err.status || 404, err)
