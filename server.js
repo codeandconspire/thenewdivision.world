d59e4de16e6f1d57bd3664d620d578b114967dd5
Carl Törnqvist <carl@tornqv.ist>
2018-06-28 15:19:43 +0200

    Add jalla

---
+++ server.js
@@ -0,0 +1,98 @@
+if (!process.env.NOW) require('dotenv/config')
+
+var url = require('url')
+var jalla = require('jalla')
+var body = require('koa-body')
+var dedent = require('dedent')
+var route = require('koa-route')
+var compose = require('koa-compose')
+var Prismic = require('prismic-javascript')
+var purge = require('./lib/purge')
+
+var PRISMIC_ENDPOINT = 'https://thenewdivision.cdn.prismic.io/api/v2'
+
+var app = jalla('index.js', {sw: 'sw.js'})
+
+app.use(route.get('/robots.txt', function (ctx, next) {
+  if (ctx.host === 'www.thenewdivision.world') return next()
+  ctx.type = 'text/plain'
+  ctx.body = dedent`
+    User-agent: *
+    Disallow: /
+  `
+}))
+
+app.use(route.post('/prismic-hook', compose([body(), async function (ctx) {
+  var secret = ctx.request.body && ctx.request.body.secret
+  ctx.assert(secret === process.env.PRISMIC_THENEWDIVISION_SECRET, 403, 'Secret mismatch')
+  return new Promise(function (resolve, reject) {
+    purge(function (err, response) {
+      if (err) return reject(err)
+      resolve()
+    })
+  })
+}])))
+
+app.use(function (ctx, next) {
+  if (!ctx.accepts('html')) return next()
+
+  var previewCookie = ctx.cookies.get(Prismic.previewCookie)
+  if (previewCookie) {
+    ctx.state.ref = previewCookie
+    ctx.set('Cache-Control', 'max-age=0')
+  } else {
+    ctx.state.ref = null
+  }
+
+  var allowCache = process.env.NODE_ENV !== 'development'
+  if (!previewCookie && allowCache && ctx.path !== '/prismic-preview') {
+    ctx.set('Cache-Control', `s-maxage=${60 * 60 * 24 * 7}, max-age=${60 * 10}`)
+  }
+
+  return next()
+})
+
+app.use(route.get('/prismic-preview', async function (ctx) {
+  var host = process.env.NOW_URL && url.parse(process.env.NOW_URL).host
+  if (host && ctx.host !== host) {
+    return ctx.redirect(url.resolve(process.env.NOW_URL, ctx.url))
+  }
+
+  var token = ctx.query.token
+  var api = await Prismic.api(PRISMIC_ENDPOINT, {req: ctx.req})
+  var href = await api.previewSession(token, resolvePreview, '/')
+
+  ctx.set('Cache-Control', 'max-age=0')
+  ctx.cookies.set(Prismic.previewCookie, token, {
+    expires: new Date(Date.now() + (1000 * 60 * 30)),
+    path: '/'
+  })
+
+  ctx.redirect(href)
+}))
+
+if (process.env.NOW && process.env.NODE_ENV === 'production') {
+  purge(['/sw.js'], function (err) {
+    if (err) throw err
+    start()
+  })
+} else {
+  start()
+}
+
+// resolve document preview url
+// obj -> str
+function resolvePreview (doc) {
+  switch (doc.type) {
+    case 'homepage': return '/'
+    case 'invite': return '/invite'
+    case 'story': return `/stories/${doc.uid}`
+    default: throw new Error('Preview not available')
+  }
+}
+
+// start server
+// () -> void
+function start () {
+  app.listen(process.env.PORT || 8080)
+}
--- sw.js
+++ sw.js
@@ -1,18 +1,18 @@
 /* eslint-env serviceworker */
 
-const TRACKING_REGEX = /https?:\/\/((www|ssl)\.)?google-analytics\.com/
-const BUILD_VERSION = process.env.npm_package_version
-const PRISMIC_ENDPOINT = 'https://thenewdivision.cdn.prismic.io'
-const IS_DEVELOPMENT = process.env.NODE_ENV === 'development'
-const FILES = [
+var TRACKING_REGEX = /https?:\/\/((www|ssl)\.)?google-analytics\.com/
+var PRISMIC_ENDPOINT = 'https://thenewdivision.cdn.prismic.io'
+var IS_DEVELOPMENT = process.env.NODE_ENV === 'development'
+var CACHE_KEY = getCacheKey()
+var FILES = [
   '/'
 ]
 
 self.addEventListener('install', function oninstall (event) {
   event.waitUntil(
     caches
-      .open(BUILD_VERSION)
-      .then((cache) => cache.addAll(FILES))
+      .open(CACHE_KEY)
+      .then(cache => cache.addAll(FILES))
       .then(() => self.skipWaiting())
   )
 })
@@ -27,40 +27,54 @@ self.addEventListener('fetch', function onfetch (event) {
   const isHTML = req.headers.get('accept').includes('text/html')
 
   event.respondWith(
-    caches.open(BUILD_VERSION).then((cache) => {
-      return cache.match(req).then((cached) => {
+    caches.open(CACHE_KEY).then(cache => {
+      return cache.match(req).then(cached => {
         const isLocal = self.location.origin === url.origin
         const isCMS = url.href.indexOf(PRISMIC_ENDPOINT) === 0
 
-        // always bypass cache for requests to application APIs
+        // bypass cache for certain types
         if ((isHTML && isLocal) || isCMS || IS_DEVELOPMENT) {
-          return update(cached)
+          return update(cache, cached)
         }
 
         // bypass cache for tracking scripts
         if (TRACKING_REGEX.test(url.href)) return self.fetch(req)
 
-        // Use cached response
-        return cached || update()
+        // use cached response
+        return cached || update(cache)
       })
-
-      function update (fallback) {
-        if (req.cache === 'only-if-cached' && req.mode !== 'same-origin') {
-          return fallback
-        }
-
-        return self.fetch(req).then((response) => {
-          if (!response.ok && fallback) return fallback
-          cache.put(req, response.clone())
-          return response
-        })
-      }
     })
   )
+
+  // fetch request and update cache
+  // (Cache, Response?) -> any
+  function update (cache, fallback) {
+    if (req.cache === 'only-if-cached' && req.mode !== 'same-origin') {
+      return fallback
+    }
+
+    return self.fetch(req).then(response => {
+      if (!response.ok && fallback) return fallback
+      cache.put(req, response.clone())
+      return response
+    })
+  }
 })
 
+// clear application cache
+// () -> Promise
 function clear () {
-  return caches.keys().then((keys) => {
-    return Promise.all(keys.map((key) => caches.delete(key)))
+  return caches.keys().then(keys => {
+    return Promise.all(keys.map(key => caches.delete(key)))
   })
+}
+
+// get application cache key
+// () -> str
+function getCacheKey () {
+  if (process.env.NOW_URL) {
+    return process.env.NOW_URL.match(/\w+(?=\.now\.sh)/)[0]
+  } else {
+    return process.env.npm_package_version
+  }
 }
