badd8ab035cf1e6b11790669d4f3e04aed8539e4
Carl Törnqvist <carl@tornqv.ist>
2022-12-05 10:45:40 +0100

    Fix SSR

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const splitRequire = require('split-require')
-const lazy = require('choo-lazy-view')
 const choo = require('choo')
+
+const SELECTOR = '#app'
 
 const app = choo()
 
@@ -22,12 +23,49 @@ app.route('/', lazy(() => splitRequire('./views/page')))
 app.route('/*', lazy(() => splitRequire('./views/page')))
 
 try {
-  module.exports = app.mount('#app')
+  module.exports = app.mount(SELECTOR)
   // remove parse guard added in header
   window.onerror = null
 } catch (err) {
   if (typeof window !== 'undefined') {
     document.documentElement.removeAttribute('scripting-enabled')
     document.documentElement.setAttribute('scripting-initial-only', '')
+  }
+}
+
+/**
+ * This is a subset of cho-lazy-view due to incompatabilities with node 16
+ * @param {Function} load Asynchronous view loading function
+ * @returns {Function}
+ */
+function lazy (load) {
+  let promise
+  let view
+
+  return function proxy (state, emit) {
+    if (view) return view(state, emit)
+
+    if (!promise) {
+      promise = load().then(function (_view) {
+        // asynchronously render view to account for nested prefetches
+        if (typeof window === 'undefined') _view(state, emit)
+        else emit('render')
+        view = _view
+      })
+      emit('prefetch', promise)
+    } else {
+      promise.then(function () {
+        emit('render')
+      })
+    }
+
+    // assuming app has been provided initialState by server side render
+    if (typeof window === 'undefined') {
+      // eslint-disable-next-line no-new-wrappers
+      const str = new String()
+      str.__encoded = true
+      return str
+    }
+    return document.querySelector(SELECTOR)
   }
 }
